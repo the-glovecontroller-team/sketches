b5b932fa9214a64e5b18179029de09bd7445e3ce
RomaSV <sve4nikov.roma@yandex.ru>
2019-09-01 17:21:50 +0300

    the 4th servo is now controlled by a pot

--- lesson-2/part-1/mearm/mearm.ino
+++ lesson-2/part-1/mearm/mearm.ino
@@ -2,12 +2,11 @@
 
 #define CLAW_SERVO_OPEN 90
 #define CLAW_SERVO_CLOSE 180
-#define UPDOWN_SERVO_UP 170
-#define UPDOWN_SERVO_DOWN 10
 
 #define BUTTON_PIN 7
 #define X_ROT_POT_PIN A0
 #define Y_ROT_POT_PIN A1
+#define UPDOWN_POT_PIN A2
 
 Servo clawServo;
 Servo upDownServo;
@@ -15,7 +14,6 @@ Servo xRotationServo;
 Servo yRotationServo;
 
 bool clawOpened;
-bool upDown; // true -> вверх(up); false -> вниз(down)
 
 void setup() {
     // Установка управляющих пинов для серв
@@ -49,16 +47,6 @@ void loop() {
     yRotationServo.write(value);
 
     // Читаем данные о движении вверх/вниз и поворачиваем серво
-    int dir = 1; // Временно запишем туда 1
-    if (dir > 0) {
-        if (!upDown) {
-            upDownServo.write(UPDOWN_SERVO_UP);
-            upDown = true;
-        }
-    } else if(dir < 0) {
-        if (upDown) {
-            upDownServo.write(UPDOWN_SERVO_DOWN);
-            upDown = false;
-        }
-    }
+    value = map(analogRead(UPDOWN_POT_PIN), 0, 1023, 0, 180);
+    upDownServo.write(value);
 }
